6c4421ceb35b7170cc625c460cc5998b52d7f572
Aryan <aryanabed48@gmail.com>
2020-05-25 18:32:49 -0400

    Replaced Spaces with Tabs

--- src/BasicClient.java
+++ src/BasicClient.java
@@ -5,58 +5,58 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class BasicClient {
-    final String LOCAL_HOST = "127.0.0.1";
-    final int PORT = 5000;
-    
-    Socket clientSocket;      //client socket for connection
-    ObjectInputStream input;     //reader for the input stream
-    ObjectOutputStream output;       //writer for the output stream
-    boolean running = true;   //program status
-    
-    public static void main (String[] args) { 
-        BasicClient client = new BasicClient();
-        client.go();
-    }
+	final String LOCAL_HOST = "127.0.0.1";
+	final int PORT = 5000;
 
-    public void go() {
-        //create a socket (try-catch required) and attempt a connection to the local IP address
-        System.out.println("Attempting to establish a connection ...");
-        try {
-            clientSocket = new Socket(LOCAL_HOST, PORT);    //create and bind a socket, and request connection
-            output = new ObjectOutputStream(clientSocket.getOutputStream());
-            input = new ObjectInputStream(clientSocket.getInputStream());
-        } catch (IOException e) {
-            System.out.println("Connection to Server Failed");
-            e.printStackTrace();
-        }
-        System.out.println("Connection to server established!");
+	Socket clientSocket;      //client socket for connection
+	ObjectInputStream input;     //reader for the input stream
+	ObjectOutputStream output;       //writer for the output stream
+	boolean running = true;   //program status
 
-        //wait for response from the server
-        while(running) {
-            try {
+	public static void main (String[] args) {
+		BasicClient client = new BasicClient();
+		client.go();
+	}
 
-                if (input.available() > 0) {
-                    Handler handler = (Handler) input.readObject();
-                    System.out.println(handler.getName());
-                    running = false;
-                }
+	public void go() {
+		//create a socket (try-catch required) and attempt a connection to the local IP address
+		System.out.println("Attempting to establish a connection ...");
+		try {
+			clientSocket = new Socket(LOCAL_HOST, PORT);    //create and bind a socket, and request connection
+			output = new ObjectOutputStream(clientSocket.getOutputStream());
+			input = new ObjectInputStream(clientSocket.getInputStream());
+		} catch (IOException e) {
+			System.out.println("Connection to Server Failed");
+			e.printStackTrace();
+		}
+		System.out.println("Connection to server established!");
+
+		//wait for response from the server
+		while(running) {
+			try {
+
+				if (input.available() > 0) {
+					Handler handler = (Handler) input.readObject();
+					System.out.println(handler.getName());
+					running = false;
+				}
 
 
-            } catch (IOException e) {
-                System.out.println("Failed to receive message from the server.");
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-        
-        //after completing the communication close all streams and sockets
-        try {  
-            input.close();
-            output.close();
-            clientSocket.close();
-        }catch (Exception e) { 
-            System.out.println("Failed to close stream or/and socket.");
-        }
-    }
+			} catch (IOException e) {
+				System.out.println("Failed to receive message from the server.");
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+		//after completing the communication close all streams and sockets
+		try {
+			input.close();
+			output.close();
+			clientSocket.close();
+		}catch (Exception e) {
+			System.out.println("Failed to close stream or/and socket.");
+		}
+	}
 }
